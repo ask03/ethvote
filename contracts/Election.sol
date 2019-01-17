4a9b83353f0edb9deda475732b8e1ea4df4d008f
Allan Kim <alkim03@gmail.com>
2019-01-17 22:37:15 +0900

    third

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,6 +7,8 @@ contract Election {
       string name;
       uint voteCount;
     }
+    //  Store accounts that have voted
+    mapping(address => bool) public voters;
     //  Store Candidates
     //  Fetch Candidate
     mapping(uint => Candidate) public candidates;
@@ -21,6 +23,17 @@ contract Election {
     function addCandidate(string memory _name) private {
       candidatesCount ++;
       candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+      // require that they haven't voted before
+      require(!voters[msg.sender]);
+      // require a valid candidate
+      require(_candidateId > 0 && _candidateId <= candidatesCount);
+      // record that voter has voted
+      voters[msg.sender] = true;
+      // update candidte votecount
+      candidates[_candidateId].voteCount++;
     }
 
 }
